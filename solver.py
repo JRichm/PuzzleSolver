68e4edc8beb947da4bf47c0ca24a5f17cfb0de37
James Richmond <jblackenmusic@gmail.com>
2023-09-12 14:42:38 -0600

    program looks for puzzle pieces

--- solver.py
+++ solver.py
@@ -1,15 +1,19 @@
 import pygame
 import easygui
+import cv2
+import numpy as np
 
 class Solver:
     def __init__(self):
-        self.menu_surface = pygame.Surface((1280, 720))
+        self.menu_surface = pygame.Surface((1650, 720))
         self.menu_surface.fill('white')    
-        self.new_image_button = Button(self, (100, 100), 'new puzzle', open_new_puzzle)
+        self.new_image_button = Button(self, (50, 100), 'open incomplete puzzle', open_new_puzzle)
         self.buttons = [self.new_image_button]
 
     def show_puzzle(self, image_url):
         puzzleIMG = pygame.image.load(image_url)
+        self.pre_process_puzzle(image_url)
+        puzzleIMG = pygame.image.load('puzzle_pieces_with_contours.jpg')
         img_w = puzzleIMG.get_width()
         img_h = puzzleIMG.get_height()
 
@@ -19,9 +23,54 @@ class Solver:
             self.puzzleIMG = pygame.transform.scale(puzzleIMG, (img_w * (400 / img_h), 400))
 
 
-        self.new_completed_image_button = Button(self, (600, 100), 'open solved reference', open_solved_pic)
+        self.new_completed_image_button = Button(self, (450, 100), 'open reference', open_solved_pic)
         self.buttons.append(self.new_completed_image_button)
-        self.menu_surface.blit(self.puzzleIMG, (100, 200))
+        self.menu_surface.blit(self.puzzleIMG, (50, 200))
+
+    def pre_process_puzzle(self, image_url):
+        puzzle_image = cv2.imread(image_url)
+
+        gray_image = cv2.cvtColor(puzzle_image, cv2.COLOR_BGR2GRAY)
+
+        _, thresholded_image = cv2.threshold(gray_image, 75, 255, cv2.THRESH_BINARY)
+
+        kernel = np.ones((5, 5), np.int8)
+        morphed_image = cv2.erode(thresholded_image, kernel, iterations=1)
+        morphed_image = cv2.dilate(morphed_image, kernel, iterations=1)
+
+        contours, _ = cv2.findContours(morphed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        min_contour_area = 1000
+        filtered_contours = [contour for contour in contours if cv2.contourArea(contour) > min_contour_area]
+
+        mask = np.zeros_like(thresholded_image)
+        cv2.drawContours(mask, filtered_contours, -1, 255, thickness=cv2.FILLED)
+
+        puzzle_pieces = cv2.bitwise_and(puzzle_image, puzzle_image, mask=mask)
+
+        cv2.imwrite('puzzle_pieces_noBG.jpg', puzzle_pieces)
+
+        puzzle_image = cv2.imread('puzzle_pieces_noBG.jpg', cv2.IMREAD_GRAYSCALE)
+
+        contours, _ = cv2.findContours(puzzle_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        contour_image = cv2.cvtColor(puzzle_image, cv2.COLOR_GRAY2BGR)
+
+        piece_index = 0
+
+        for contour in contours:
+            x,y,w,h = cv2.boundingRect(contour)
+
+            piece = puzzle_image[y:y+h, x:x+w]
+
+            piece_filename = f'/pieces/piece_{piece_index}.jpg'
+            
+            cv2.imwrite(piece_filename, piece)
+
+            cv2.rectangle(contour_image, (x, y), (x + w, y + h), (0, 255, 0))
+
+            piece_index += 1
+            
+        cv2.imwrite('puzzle_pieces_with_contours.jpg', contour_image)
 
     def show_double_view(self, solved_image_url):
         solved_puzzleIMG = pygame.image.load(solved_image_url)
@@ -29,12 +78,12 @@ class Solver:
         img_h = solved_puzzleIMG.get_height()
         
         if img_w > img_h:
-            self.solved_puzzleIMG = pygame.transform.scale(solved_puzzleIMG, (400, img_h * (400 / img_w)))
+            self.solved_puzzleIMG = pygame.transform.scale(solved_puzzleIMG, (300, img_h * (300 / img_w)))
         else:
-            self.solved_puzzleIMG = pygame.transform.scale(solved_puzzleIMG, (img_w * (400 / img_h), 400))
+            self.solved_puzzleIMG = pygame.transform.scale(solved_puzzleIMG, (img_w * (300 / img_h), 300))
 
-        self.menu_surface.blit(self.puzzleIMG, (100, 200))
-        self.menu_surface.blit(self.solved_puzzleIMG, (600, 200))
+        self.menu_surface.blit(self.puzzleIMG, (50, 200))
+        self.menu_surface.blit(self.solved_puzzleIMG, (450, 200))
 
     def show(self, window):
         window.blit(self.menu_surface, (0, 0))
